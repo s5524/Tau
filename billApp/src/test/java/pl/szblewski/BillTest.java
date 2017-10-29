576c713954e1ccb74ecc4aad91b6f238a2d15376
s5524 <szymon.zblewski@gmail.com>
2017-10-29 07:41:49 +0100

    Add mocks

--- billApp/src/test/java/pl/szblewski/BillTest.java
+++ billApp/src/test/java/pl/szblewski/BillTest.java
@@ -1,20 +1,28 @@
 package pl.szblewski.service;
 
-import org.junit.*;
+import org.junit.runner.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 import pl.szblewski.domain.Bill;
-
+import org.junit.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.mockito.ArgumentMatcher.*;
 import static org.mockito.BDDMockito.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 
+
+@RunWith(MockitoJUnitRunner.class)
 public class BillTest {
+
+    @Mock
+
     private BillService billService;
     private BillDataAccess billDataAccess;
 
@@ -38,10 +46,41 @@ public class BillTest {
 
         String pat = "\\w{5}";
         List<Bill> results = billDataAccess.findBillsByRegex(pat);
-
         Assert.assertNotNull(results);
         Assert.assertEquals(2,results.size());
+        //Mockito.verify(billService,times(1)).deleteBill(billToDelete1);
+
     }
+
+
+    @Test
+    public void deleteBillTest(){
+        BillOrganiser bo = new BillOrganiser();
+        Bill billToDelete1 = new Bill("Object1",1);
+        Bill billToDelete2 = new Bill("Object2",1);
+        bo.createBill(billToDelete1);
+        bo.createBill(billToDelete2);
+
+
+
+        Mockito.verify(billService,times(1)).deleteBill(billToDelete1);
+        verify(billService,times(1)).deleteBill(billToDelete2);
+
+    }
+
+    @Test
+    public void deleteBillTestException(){
+        Bill bill = new Bill("Test",22);
+        List<Bill> bills = new ArrayList<Bill>();
+        bills.add(bill);
+        willThrow(NoSuchElementException.class).given(billService).deleteBill(any(Bill.class));
+
+        billDataAccess.deleteBills(bills);
+
+
+    }
+
+
 
 
 
