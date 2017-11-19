5cb496430d0b0139f467dfae1d5e7a0f84447ae6
s5524 <szymon.zblewski@gmail.com>
2017-11-19 11:11:08 +0100

    Update

--- billApp/src/test/java/bdd/CheckNameByRegexStep.java
+++ billApp/src/test/java/bdd/CheckNameByRegexStep.java
@@ -1,27 +1,17 @@
 package bdd;
 
-import cucumber.api.PendingException;
-import cucumber.api.java.Before;
-import cucumber.api.java.en.But;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.Assert;
-import org.mockito.internal.util.collections.Iterables;
 import pl.szblewski.domain.Bill;
 import pl.szblewski.service.BillDataAccess;
 import pl.szblewski.service.BillOrganiser;
 import pl.szblewski.service.BillService;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 
 public class CheckNameByRegexStep {
